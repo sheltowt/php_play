229c1bf34b4f833b20e7e5166c7e3df2a920d919
billiamwilliam <sheltowt@gmail.com>
2015-03-07 19:30:17 -0800

    adding more functions to the twitter class

--- classes/twitter.php
+++ classes/twitter.php
@@ -19,6 +19,38 @@
 			return $arrayNames;
 		}
 		
+		public function returnDesiredValues(){
+			$desiredValues = [];
+			foreach($this->data as $key => $tweet){
+				var_dump($tweet);
+				$tweetValues = [];
+				foreach($tweet as $key => $value){
+					switch ($key){
+						case "text":
+							$tweetValues["text"] = $value;
+							break;
+						case "user_id":
+							$tweetValues["user_id"] = $value;
+							break;
+						case "id_str":
+							$tweetValues["id_str"] = $value;
+							break;
+						case "created_at":
+							$tweetValues["created_at"] = $value;
+							break;
+						case "url":
+							$tweetValues["url"] = $value[0];
+							break;
+						default:
+							break;
+					}
+				}
+				array_push($desiredValues, $tweetValues);
+			}			
+			
+			return $desiredValues;
+		}
+		
 		
 	}
 ?>
--- php_twitter.php
+++ php_twitter.php
@@ -1,5 +1,6 @@
 <?php
-	require 'vendor/autoload.php';
+	require_once 'vendor/autoload.php';
+	require_once 'twitter/generated-conf/config.php';
 	require 'classes/twitter.php';
 	
 	$string = file_get_contents("settings.json");
@@ -18,10 +19,6 @@
 	$twitterData = json_decode($response);
 
 	$twitterApi = new TwitterApi($twitterData);
-	$names = $twitterApi->extractText();
+	$names = $twitterApi->returnDesiredValues();
 	var_dump($names);
-	$author = new Author();
-	$author->setFirstName('Jane');
-	$author->setLastName('Austen');
-	$author->save()
 ?>
